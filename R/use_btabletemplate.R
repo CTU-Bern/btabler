d824bcb9dbab7623c8cf8543976fef9b3dec904b
aghaynes <aghaynes@gmail.com>
2021-05-07 09:26:12 +0200

    add basic templates

---
+++ R/use_btabletemplate.R
@@ -0,0 +1,31 @@
+#' Open a basic template suitable for use with btable
+#'
+#' @param name name to save the file under
+#' @param fmt character defining format (\code{Rmd} or \code{Rnw} are accepted)
+#' @param ... other options passed to \code{\link[usethis]{use_template}}
+#' @param open logical, whether to open the file
+#'
+#' @return (by default) saves and opens a script of the chosen format
+#' @export
+#'
+#' @examples
+#' # Rmd file
+#' use_btabletemplate("foo")
+#' # Rnw file
+#' use_btabletemplate("foo", fmt = "Rnw")
+use_btabletemplate <- function(name
+                               , fmt = "Rmd"
+                               , ...
+                               , open = TRUE
+                               ) {
+
+  fmt <- match.arg(fmt, c("Rmd", "Rnw"))
+
+  usethis::use_template(template = paste0("template.", fmt)
+                        , save_as = name
+                        , package = "btabler"
+                        , open = open
+                        , ...
+                        )
+
+}
